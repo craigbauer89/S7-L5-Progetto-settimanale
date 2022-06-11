57d62b706104d740c177afe3234665a4b0488767
craigbauer89 <craigbauer89@gmail.com>
2022-06-11 13:45:26 +0200

    update 1

--- client/js/script.js
+++ client/js/script.js
@@ -33,14 +33,28 @@ function userlist(users) {
     let list = document.querySelector('.container');
         users.forEach(element => {
             let card = document.createElement('div');
-            card.innerHTML = `
-                            <img src='img/person.png'>
-                            <h2>${element.firstName} ${element.lastName}</h2>
-                            <p>${element.email}</p>
+            card.innerHTML = 
+                            // `
+                            // <img src='img/person.png'>
+                            // <h2>${element.firstName} ${element.lastName}</h2>
+                            // <p>${element.email}</p>
                             
-                            <td><button type="button" onclick="details(${element.id})" class="btn btn-outline-danger btn-sm">More Details</button></td>
-                            <div class="container${element.username}"><div>
+                            // <td><button type="button" onclick="details(${element.id})" class="btn btn-outline-danger btn-sm">More Details</button></td>
+                            // <div class="container${element.username}"><div>
+                            // `
                             `
+                            <div class="col">
+                                <div class="card">
+                                    <img src="img/person.png" class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                        <h5 class="card-title">${element.firstName} ${element.lastName}</h5>
+                                        <p class="card-text">${element.email}</p>
+                                        <button type="button" onclick="details(${element.id})" class="btn btn-sm">More Details</button>
+                                    </div>
+                                </div>
+                            </div>
+                            `
+
             list.appendChild(card);
             console.log(element.username);
         });
@@ -88,11 +102,25 @@ function details(id) {
                         
                         if (id === newEL_id) {
                             let card = document.createElement('div'+id);
-                            card.innerHTML = `
-                                    <h2>${element.firstName} ${element.lastName} Details</h2>
-                                    <p>Gender: ${element.gender}</p>
-                                    <p>Username: ${element.username}</p>
-                                    <td><button type="button" onclick="removeUser(${element.id})" class="btn btn-outline-danger btn-sm">Clear</button></td>
+                             card.innerHTML = //`
+                            //         <h2>${element.firstName} ${element.lastName} Details</h2>
+                            //         <p>Gender: ${element.gender}</p>
+                            //         <p>Username: ${element.username}</p>
+                            //         <td><button type="button" onclick="removeUser(${element.id})" class="btn btn-outline-danger btn-sm">Clear</button></td>
+                            //         `
+                                    `<div class="row row-cols-1 row-cols-lg-3 g-4">
+                                    <div class="col">
+                                        <div class="card">
+                                            <img src="${element.profileURL}" class="card-img-top" alt="...">
+                                            <div class="card-body">
+                                                <h5 class="card-title">${element.firstName} ${element.lastName}</h5>
+                                                <p class="card-text">Gender: ${element.gender}</p>
+                                                <p class="card-text">$Email: ${element.email}</p>
+                                                <p class="card-text">Username: ${element.username}</p>
+                                                <button type="button" onclick="removeUser(${element.id})" class="btn  btn-sm">Clear</button>
+                                            </div>
+                                        </div>
+                                    </div>
                                     `
                             list.appendChild(card);
                         }
